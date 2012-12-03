1d56ede390c2188f6b9710a7a13b93168df4d825
Maxime Menant <menant.maxime@gmail.com>
2012-12-03 20:51:29 +0100

    Init repository for the FB_PagesFetcher Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbPagesfetcher::Application.config.secret_token = '3e60a0041a9e07f3283762aa10a5f38f2aa9426acdee7b2f57bbf905b38c6ba7213363cda14bdc5f381282688a541ed6bde63848538c24bfc2f15e6bf1119ba7'
